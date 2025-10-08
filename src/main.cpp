e20d33363c3083dd7253c390b2c2b209987e225c
Jemooooooo2344 <jemoo2345@gmail.com>
2025-10-07 22:08:25 -0600

    Changed main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,6 @@
 #include <string>
 
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-    std::cout << "Hello, " << name << "!" << std::endl;
+   
     return 0;
 }
