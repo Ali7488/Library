5d4a7d73f6db122363c2146705e6c6b86f0789f1
Mohannad Ali <mohannad.eali@gmail.com>
2025-10-12 23:36:38 -0600

    added csv file for testing, and created the template for the BookManagement system

--- Include/BookManager.h
+++ Include/BookManager.h
@@ -1,10 +1,33 @@
 /********************************
- * 
- * 
- * 
- * 
+ * Purpose: Manages a collection of book objects
+ * variables: unordered_map<ID, Book> collection
+ * methods: addBook, removeBook, findBook, borrowBookbyID,
+ * returnBookbyID, displayAll, saveFile, LoadFile
 ********************************/
 #ifndef BOOKMANAGER_H
 #define BOOKMANAGER_H
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <fstream>
+#include "../Include/Book.h"
+
+using BookMap = std::unordered_map<int, Book>;
+class BookManager{
+    private:
+    //hash map for storage of different book objects with the key being
+    //a type int, and the value being type book.
+    BookMap collection;
+    public:
+    BookManager();
+    void addBook(Book newBook);
+    void removeBook(Book removal);
+    Book findBook(int id);
+    void borrowBookbyID(int id);
+    void returnBookbyID(int id);
+    void displayAll();
+    void saveFile();
+    void loadFile();
+};
 
 #endif
